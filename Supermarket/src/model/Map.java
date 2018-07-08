4b2ba69537f4389763ff76008ff3e31952ef3aa7
GleSha <gleb.shaforostov@yandex.ru>
2018-07-08 22:30:26 +0700

    Final version

--- Supermarket/src/model/Map.java
+++ Supermarket/src/model/Map.java
@@ -1,9 +1,6 @@
 package model;
 
-
-import javafx.scene.control.Alert;
 import javafx.util.Pair;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
--- Supermarket/src/model/Product.java
+++ Supermarket/src/model/Product.java
@@ -5,10 +5,19 @@ import javafx.util.Pair;
 
 public class Product {
 
+    /**
+     * Название продукта
+     * */
     private String name;
 
+    /**
+     * уникальный номер продукта
+     * */
     private byte ID;
 
+    /**
+     * цена продукта
+     * */
     private int cost;
 
     public Product(byte ID, Pair<Integer, String> pair) {
